71c11d5161476d193e733c442c92aeb37314bdb4
Autumn <439661734@qq.com>
2022-02-25 14:14:15 +0800

    chore: set tsconfig

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  '*.{js,ts}': ['pnpm run lint:eslint', 'pnpm run lint:prettier', 'pnpm run test'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-    'pnpm run lint:prettier --parser json'
-  ],
-  'package.json': ['pnpm run lint:prettier'],
+  // '*.{js,ts}': ['pnpm run lint:eslint', 'pnpm run lint:prettier', 'pnpm run test'],
+  '*.{js,ts}': ['pnpm run lint:eslint', 'pnpm run lint:prettier'],
+  'package.json': ['pnpm run lint:prettier']
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import sum from './sum'
 
 console.log(sum(1, 2))
+const a = {}
+export { a }
