11aa6389d20485bbf8365d6247c7b7ba6757a64e
Jin <439661734@qq.com>
2022-09-29 22:09:16 +0800

    chore: delete chinese and fix bug

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
-import typescript from '@rollup/plugin-typescript' // 让 rollup 认识 ts 的代码
+import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json'
 
-// 为了将引入的 npm 包，也打包进最终结果中
 import resolve from '@rollup/plugin-node-resolve'
-// import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import externals from 'rollup-plugin-node-externals'
 
-// 一段自定义的内容，以下内容会添加到打包结果中
 const footer = `
 if(typeof window !== 'undefined') {
   window._VERSION_ = '${pkg.version}'
